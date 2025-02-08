5a48e4f33375ef9bfd8c173b43c66a05b230d180
Andre <andreferraz@ferrazbrasil.com.br>
2025-02-08 01:33:43 -0500

    added enemy, need enemy_move()

--- videogame/videogame.c
+++ videogame/videogame.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "pico/rand.h"
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/gpio.h"
@@ -26,53 +27,6 @@ struct player{
 
 };
 
-void init_gpios();
-void i2c_setup();
-
-void player_move(uint16_t x_mov, uint16_t y_mov, struct player *p1, bool dash, bool stop);
-
-
-int main()
-{
-    stdio_init_all();
-    init_gpios();
-    i2c_setup();
-    adc_init();
-
-    adc_gpio_init(XJOYSTICK);
-    adc_gpio_init(YJOYSTICK);
-    adc_set_round_robin(3);
-
-    bool a_pressed, b_pressed, c_pressed, d_pressed = false;
-    uint16_t x_read, y_read = 0;
-
-    struct player p1 = {0,0};
-
-    while (true) {
-        a_pressed = gpio_get(GREENBUTTON);
-        b_pressed = gpio_get(YELLOWBUTTON);
-        c_pressed = gpio_get(REDBUTTON);
-        d_pressed = gpio_get(BLUEBUTTON);
-
-        x_read = adc_read();
-        y_read = adc_read();
-
-        int ret;
-        uint8_t rxdata;
-
-        player_move(x_read, y_read, &p1, d_pressed, b_pressed);
-
-        ret = i2c_read_blocking(i2c_default, SCREEN_ADDRESS, &rxdata, 1, false);
-
-        ret > 0 ? printf("I2C Display found ") : printf("I2C Display not found ");
-        printf("GREEN:%d YELLOW:%d RED:%d BLUE:%d X:%d Y:%d\n", a_pressed, b_pressed, c_pressed, d_pressed, x_read, y_read);
-        printf("Player pos: x = %d y = %d\n", p1.x_pos, p1.y_pos);
-        sleep_ms(500);
-    }
-}
-
-
-
 void init_gpios(){
 
     gpio_init(GREENBUTTON);
@@ -98,20 +52,21 @@ void i2c_setup(){
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
 }
 
+
 void player_move(uint16_t x_mov, uint16_t y_mov, struct player *p1, bool dash, bool stop){
     if(!stop){
         uint8_t dash_multiplier = dash ? 3 : 1; 
 
         if(x_mov < DEADZONELOWERSIDE){
 
-            if(p1->x_pos - dash_multiplier >= 0){
+            if(p1->x_pos - dash_multiplier >= 2){
                 p1->x_pos -= dash_multiplier;
             }
 
         } 
         if(x_mov > DEADZONEHIGHERSIDE) {
 
-            if (p1->x_pos + dash_multiplier <= 62){
+            if (p1->x_pos + dash_multiplier <= 126){
                 p1->x_pos += dash_multiplier;
             }
         
@@ -119,14 +74,14 @@ void player_move(uint16_t x_mov, uint16_t y_mov, struct player *p1, bool dash, b
 
         if(y_mov > DEADZONELOWERSIDE){
 
-            if(p1->y_pos - dash_multiplier >= 0){
+            if(p1->y_pos - dash_multiplier >= 2){
                 p1->y_pos -= dash_multiplier;
             }
 
         } 
         if(y_mov < DEADZONEHIGHERSIDE) {
 
-            if (p1->y_pos + dash_multiplier < 126){
+            if (p1->y_pos + dash_multiplier < 62){
                 p1->y_pos += dash_multiplier;
             }
         
@@ -135,4 +90,59 @@ void player_move(uint16_t x_mov, uint16_t y_mov, struct player *p1, bool dash, b
     
     
     
+}
+
+uint8_t rand_mapped(char dir){
+
+    uint8_t max = dir == 'x' ? 62 : 126;
+
+    return (uint8_t)(get_rand_32() % (max - 2 + 1) + 2);
+
+}
+
+
+int main()
+{
+    stdio_init_all();
+    init_gpios();
+    i2c_setup();
+    adc_init();
+
+    adc_gpio_init(XJOYSTICK);
+    adc_gpio_init(YJOYSTICK);
+    adc_set_round_robin(3);
+
+    bool a_pressed, b_pressed, c_pressed, d_pressed = false;
+    uint16_t x_read, y_read = 0;
+
+    struct player p1 = {0,0};
+    struct player enemy1 = {rand_mapped('x'),rand_mapped('y')};
+    struct player enemy2 = {rand_mapped('x'),rand_mapped('y')};
+
+    while (true) {
+        a_pressed = gpio_get(GREENBUTTON);
+        b_pressed = gpio_get(YELLOWBUTTON);
+        c_pressed = gpio_get(REDBUTTON);
+        d_pressed = gpio_get(BLUEBUTTON);
+
+        x_read = adc_read();
+        y_read = adc_read();
+
+        int ret;
+        uint8_t rxdata;
+
+        player_move(x_read, y_read, &p1, d_pressed, b_pressed);
+        player_move(rand_mapped('x'), rand_mapped('y'), &enemy1, true, false);
+        player_move(rand_mapped('x'), rand_mapped('y'), &enemy2, true, false);
+        
+
+        ret = i2c_read_blocking(i2c_default, SCREEN_ADDRESS, &rxdata, 1, false);
+
+        ret > 0 ? printf("I2C Display found ") : printf("I2C Display not found ");
+        printf("GREEN:%d YELLOW:%d RED:%d BLUE:%d X:%d Y:%d\n", a_pressed, b_pressed, c_pressed, d_pressed, x_read, y_read);
+        printf("Player 1 pos: x = %d y = %d\n", p1.x_pos, p1.y_pos);
+        printf("Player 1 pos: x = %d y = %d\n", enemy1.x_pos, enemy1.y_pos);
+        printf("Player 1 pos: x = %d y = %d\n", enemy1.x_pos, enemy2.y_pos);
+        sleep_ms(500);
+    }
 }
