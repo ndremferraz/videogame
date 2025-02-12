00748996565771f3bc59ec5896345609d33ace74
Andre <andreferraz@ferrazbrasil.com.br>
2025-02-11 19:06:18 -0500

    ssd1306.c added

---
+++ videogame/ssd1306.c
@@ -0,0 +1,152 @@
+#include "ssd1306.h"
+
+//CLK SPEED DEFINITION FOR I2C
+#define SSD1306_I2C_CLK             400
+
+//LIST OF COMMANDS LOOK AT SSD1306 for greater details
+#define SSD1306_SET_MEM_MODE        0x20
+#define SSD1306_SET_COL_ADDR        0x21
+#define SSD1306_SET_PAGE_ADDR       0x22
+#define SSD1306_SET_HORIZ_SCROLL    0x26
+#define SSD1306_SET_SCROLL          0x2E
+
+#define SSD1306_SET_DISP_START_LINE 0x40
+
+#define SSD1306_SET_CONTRAST        0x81
+#define SSD1306_SET_CHARGE_PUMP     0x8D
+
+#define SSD1306_SET_SEG_REMAP       0xA0
+#define SSD1306_SET_ENTIRE_ON       0xA4
+#define SSD1306_SET_ALL_ON          0xA5
+#define SSD1306_SET_NORM_DISP       0xA6
+#define SSD1306_SET_INV_DISP        0xA7
+#define SSD1306_SET_MUX_RATIO       0xA8
+#define SSD1306_SET_DISP            0xAE
+#define SSD1306_SET_COM_OUT_DIR     0xC0
+#define SSD1306_SET_COM_OUT_DIR_FLIP 0xC0
+
+#define SSD1306_SET_DISP_OFFSET     0xD3
+#define SSD1306_SET_DISP_CLK_DIV    0xD5
+#define SSD1306_SET_PRECHARGE       0xD9
+#define SSD1306_SET_COM_PIN_CFG     0xDA
+#define SSD1306_SET_VCOM_DESEL      0xDB
+
+#define SSD1306_PAGE_HEIGHT         8
+#define SSD1306_NUM_PAGES           SSD1306_HEIGHT / SSD1306_PAGE_HEIGHT
+#define SSD1306_BUF_LEN             SSD1306_NUM_PAGES * SSD1306_WIDTH
+
+#define SSD1306_WRITE_MODE         0xFE
+#define SSD1306_READ_MODE          0xFF
+
+
+//calculates the length of a buffer to be the total area of the screen 
+void calc_render_area_buflen(struct render_area *area) {
+    
+    area->buflen = (area->end_col - area->start_col + 1) * (area->end_page - area->start_page + 1);
+}
+
+
+void SSD1306_send_cmd(uint8_t cmd) {
+    // I2C write process expects a control byte followed by data
+    // this "data" can be a command or data to follow up a command
+    // Co = 1, D/C = 0 => the driver expects a command
+    uint8_t buf[2] = {0x80, cmd};
+    #ifdef SSD13_I2C_ADDR
+    i2c_write_blocking(i2c_default, SSD1306_I2C_ADDR, buf, 2, false);
+    #endif
+
+}
+
+//sends multiple commands
+void SSD1306_send_cmd_list(uint8_t *buf, int num) {
+    for (int i=0;i<num;i++)
+        SSD1306_send_cmd(buf[i]);
+}
+
+//creates a dynamic allocation to store the buffer and makes first address a control byte
+//copies memory from the buffer sent to the new buffer
+//sends buffer and frees it
+void SSD1306_send_buf(uint8_t buf[], int buflen) {
+    // in horizontal addressing mode, the column address pointer auto-increments
+    // and then wraps around to the next page, so we can send the entire frame
+    // buffer in one gooooooo!
+
+    // copy our frame buffer into a new buffer because we need to add the control byte
+    // to the beginning
+
+    uint8_t *temp_buf = malloc(buflen + 1);
+
+    temp_buf[0] = 0x40;
+    memcpy(temp_buf+1, buf, buflen);
+    #ifdef SSD1306_I2C_ADDR
+    i2c_write_blocking(i2c_default, SSD1306_I2C_ADDR, temp_buf, buflen + 1, false);
+    #endif
+
+    free(temp_buf);
+}
+
+void SSD1306_init() {
+    // Some of these commands are not strictly necessary as the reset
+    // process defaults to some of these but they are shown here
+    // to demonstrate what the initialization sequence looks like
+    // Some configuration values are recommended by the board manufacturer
+
+    uint8_t cmds[] = {
+        SSD1306_SET_DISP,               // set display off
+        /* memory mapping */
+        SSD1306_SET_MEM_MODE,           // set memory address mode 0 = horizontal, 1 = vertical, 2 = page
+        0x00,                           // horizontal addressing mode
+        /* resolution and layout */
+        SSD1306_SET_DISP_START_LINE,    // set display start line to 0
+        SSD1306_SET_SEG_REMAP | 0x01,   // set segment re-map, column address 127 is mapped to SEG0
+        SSD1306_SET_MUX_RATIO,          // set multiplex ratio
+        #ifdef SSD1306
+        SSD1306_HEIGHT - 1,             // Display height - 1
+        #endif
+        SSD1306_SET_COM_OUT_DIR | 0x08, // set COM (common) output scan direction. Scan from bottom up, COM[N-1] to COM0
+        SSD1306_SET_DISP_OFFSET,        // set display offset
+        0x00,                           // no offset
+        SSD1306_SET_COM_PIN_CFG,        // set COM (common) pins hardware configuration. Board specific magic number.
+                                        // 0x02 Works for 128x32, 0x12 Possibly works for 128x64. Other options 0x22, 0x32
+#if ((SSD1306_WIDTH == 128) && (SSD1306_HEIGHT == 32))
+        0x02,
+#elif ((SSD1306_WIDTH == 128) && (SSD1306_HEIGHT == 64))
+        0x12,
+#else
+        0x02,
+#endif
+        /* timing and driving scheme */
+        SSD1306_SET_DISP_CLK_DIV,       // set display clock divide ratio
+        0x80,                           // div ratio of 1, standard freq
+        SSD1306_SET_PRECHARGE,          // set pre-charge period
+        0xF1,                           // Vcc internally generated on our board
+        SSD1306_SET_VCOM_DESEL,         // set VCOMH deselect level
+        0x30,                           // 0.83xVcc
+        /* display */
+        SSD1306_SET_CONTRAST,           // set contrast control
+        0xFF,
+        SSD1306_SET_ENTIRE_ON,          // set entire display on to follow RAM content
+        SSD1306_SET_NORM_DISP,           // set normal (not inverted) display
+        SSD1306_SET_CHARGE_PUMP,        // set charge pump
+        0x14,                           // Vcc internally generated on our board
+        SSD1306_SET_SCROLL | 0x00,      // deactivate horizontal scrolling if set. This is necessary as memory writes will corrupt if scrolling was enabled
+        SSD1306_SET_DISP | 0x01, // turn display on
+    };
+
+    SSD1306_send_cmd_list(cmds, count_of(cmds));
+}
+
+void render(uint8_t *buf, struct render_area *area) {
+    // update a portion of the display with a render area
+    uint8_t cmds[] = {
+        SSD1306_SET_COL_ADDR,
+        area->start_col,
+        area->end_col,
+        SSD1306_SET_PAGE_ADDR,
+        area->start_page,
+        area->end_page
+    };
+
+    SSD1306_send_cmd_list(cmds, count_of(cmds));
+    SSD1306_send_buf(buf, area->buflen);
+}
